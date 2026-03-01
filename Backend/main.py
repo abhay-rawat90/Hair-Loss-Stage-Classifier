6c218fe5978eda0a736db53e206f81682b093ee1
Abhay Rawat <abhayrawat170@gmail.com>
2026-03-01 19:20:18 +0530

    Changed .pth model to .onnx model to save memory

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,12 @@
 import io
-import torch
-import torch.nn as nn
-from torchvision import transforms, models
+import numpy as np
+import onnxruntime as ort
 from PIL import Image
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 
-MODEL_PATH = "models/stage_classifier.pth"
+ONNX_PATH = "models\stage_classifier.onnx" 
 NUM_CLASSES = 7
 
 STAGE_LABELS = {
@@ -20,9 +19,7 @@ STAGE_LABELS = {
     6: "Stage 7 - Extreme Baldness"
 }
 
-
 app = FastAPI(title="Hair Loss Stage Classifier API")
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -33,38 +30,47 @@ app.add_middleware(
 )
 
 
-model = models.resnet50(pretrained=False)
-model.fc = nn.Sequential(
-    nn.Dropout(0.5),
-    nn.Linear(model.fc.in_features, NUM_CLASSES)
-)
-model.load_state_dict(torch.load(MODEL_PATH, map_location="cpu"))
-model.eval()
+session = ort.InferenceSession(ONNX_PATH)
+input_name = session.get_inputs()[0].name
 
+def preprocess_image(image: Image.Image) -> np.ndarray:
+    
+    image = image.resize((224, 224))
+    
+    img_data = np.array(image).astype(np.float32) / 255.0
+    
+    mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+    std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+    img_data = (img_data - mean) / std
+    
+    img_data = np.transpose(img_data, (2, 0, 1))
+    
+    img_data = np.expand_dims(img_data, axis=0)
+    
+    return img_data.astype(np.float32)
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406],
-                         [0.229, 0.224, 0.225])
-])
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=1)
 
 @app.post("/predict/")
 async def predict_image(file: UploadFile = File(...)):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    
+    
+    input_tensor = preprocess_image(image)
 
-    input_tensor = transform(image).unsqueeze(0)
-
-    with torch.no_grad():
-        output = model(input_tensor)
-        probabilities = torch.softmax(output, dim=1)
-        confidence, predicted_class = torch.max(probabilities, 1)
-
-    stage = STAGE_LABELS[predicted_class.item()]
-    conf = confidence.item() * 100
+    
+    outputs = session.run(None, {input_name: input_tensor})
+    logits = outputs[0]
+    
+    
+    probabilities = softmax(logits)
+    predicted_class = np.argmax(probabilities, axis=1)[0]
+    confidence = probabilities[0][predicted_class] * 100
 
     return {
-        "predicted_stage": stage,
-        "confidence": f"{conf:.1f}%"
+        "predicted_stage": STAGE_LABELS[predicted_class],
+        "confidence": f"{confidence:.1f}%"
     }
